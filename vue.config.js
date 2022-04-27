f71e7254f0a092a31cd83df26adaef467374818c
JungWoo Ham <evaham@naver.com>
2022-04-27 13:58:49 +0900

    20220427_3

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
   // github page
-  publicPath: "/app_ui_vue3/",
+  publicPath: "/app_ui_vue2/",
   outputDir: "./docs",
 });
