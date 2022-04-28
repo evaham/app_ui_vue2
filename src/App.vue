d5fd4de3b3f36cc3b1dc7571367447247f41dbd7
JungWoo Ham <evaham@naver.com>
2022-04-28 15:48:28 +0900

    20220428_1

--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,3 @@ export default {
   }),
 };
 </script>
-<style lang="scss">
-#app {
-  font-family: "Spoqa Han Sans Neo", Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-</style>
